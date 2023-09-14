67023062463e6320b04e06d30803e70ae5e930d6
Srujan-rai <raisrujan@gmail.com>
2023-09-14 19:29:18 +0530

    major chances

--- app.py
+++ app.py
@@ -1,28 +1,57 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from docx2pdf import convert
+import io
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def convert_word_to_pdf():
+def index():
     if request.method == "POST":
-        # Handle file upload
-        uploaded_file = request.files["word_file"]
+        uploaded_file = request.files["doc_file"]
         if uploaded_file.filename != "":
-            # Save the uploaded file to a temporary location
-            uploaded_file.save("temp.docx")
+            # Read the DOCX file into memory
+            docx_contents = uploaded_file.read()
 
-            # Convert the Word file to PDF
-            convert("temp.docx")
+            # Perform the conversion from DOCX to PDF in-memory
+            pdf_contents = convert_from_docx(docx_contents)
 
             # Provide a download link for the PDF
-            return send_file("temp.pdf", as_attachment=True, download_name="converted.pdf")
+            return send_pdf(pdf_contents)
 
     return render_template("index.html")
 
-if __name__ == '__main__':
-    # Use Gunicorn as the web server
-    import os
-    host = '0.0.0.0'
-    port = int(os.environ.get('PORT', 8000))  # Use the PORT environment variable provided by Render
-    app.run(debug=False, host=host, port=port)
+def convert_from_docx(docx_contents):
+    # Create a temporary file to hold the DOCX data
+    temp_docx_path = "temp.docx"
+    with open(temp_docx_path, "wb") as temp_docx_file:
+        temp_docx_file.write(docx_contents)
+
+    # Perform the conversion from DOCX to PDF
+    convert(temp_docx_path)
+
+    # Read the converted PDF data
+    pdf_contents = None
+    with open("temp.pdf", "rb") as pdf_file:
+        pdf_contents = pdf_file.read()
+
+    # Clean up temporary files
+    os.remove(temp_docx_path)
+    os.remove("temp.pdf")
+
+    return pdf_contents
+
+def send_pdf(pdf_contents):
+    # Create a BytesIO object from the PDF data
+    pdf_file = io.BytesIO(pdf_contents)
+
+    # Provide a download link for the PDF
+    return send_file(
+        pdf_file,
+        as_attachment=True,
+        download_name="rudra.pdf",
+        mimetype="application/pdf",
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
