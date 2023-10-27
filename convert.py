c6a95edbc3e31d0d6b3b7b5a4e9937d4851cc982
Srujan rai <raisrujan@gmail.com>
2023-10-27 16:51:42 +0530

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,36 @@
+from http.server import SimpleHTTPRequestHandler
+import http.server
+import socketserver
+from pdf2docx import Converter
+import shutil
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+
+        # Save the uploaded PDF file
+        with open('uploaded.pdf', 'wb') as pdf_file:
+            pdf_file.write(post_data)
+
+        # Convert the PDF to DOCX
+        cv = Converter('uploaded.pdf')
+        cv.convert('converted.docx', start=0, end=None)
+        cv.close()
+
+        # Send the converted DOCX file to the client
+        self.send_response(200)
+        self.send_header('Content-Disposition', 'attachment; filename=converted.docx')
+        self.send_header('Content-type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+        self.end_headers()
+
+        with open('converted.docx', 'rb') as docx_file:
+            shutil.copyfileobj(docx_file, self.wfile)
+
+if __name__ == '__main__':
+    port = 5000
+    Handler = MyHandler
+
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+        print(f"Serving at port {port}")
+        httpd.serve_forever()
